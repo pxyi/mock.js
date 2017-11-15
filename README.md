324201a5e9d0e072ff900e46e2d8c673cbb946e7
phuhoang <phuhoang@sina.com>
2017-11-15 09:49:41 +0800

    demo示例

--- README.md
+++ README.md
@@ -10,4 +10,21 @@ npm install
 
 # serve with hot reload at localhost:2800
 npm start
+
+# 用浏览器打开 localhost:2800/get/api 是否返回
+{
+	code: 1000,
+	message: 'get请求成功'
+}
+```
+
+
+使用方法: 直接在 routes 文件夹下新建文件
+
+``` bash
+#新建文件
+cd routes && cd .> test.js
+
+#定义接口
+参考routes/demo.js
 ```
--- routes/demo.js
+++ routes/demo.js
@@ -1,11 +1,34 @@
-const Api = async (ctx, next) => {
+const GetAPI = async (ctx, next) => {
 	ctx.body = {
 		code: 1000,
-		message: '请求成功'
+		message: 'get请求成功'
 	}
 }
 
+const PostAPI = async (ctx, next) => {
+	ctx.body = {
+		code: 1000,
+		message: 'post请求成功'
+	}
+}
 
+const AllAPI = async (ctx, next) => {
+	ctx.body = {
+		code: 1000,
+		message: 'get/post均可请求成功'
+	}
+}
+
+/**
+ *	@module 可以定义以下三种方法								<空格不可缺少>
+ *	@param	GET + 空格 +	请求地址							<可使用get方法请求>
+ *	@param	POST + 空格 +	请求地址  					<可使用post方法请求>
+ *	@param	ALL + 空格 +	请求地址 							<get/post均可以>
+ *  ----------------------------------------------------------
+ *	@description: 变量必须为 async 函数
+ */
 module.exports = {
-	'GET /api': Api
+	'GET /get/api': GetAPI,
+	'POST /post/api': PostAPI,
+	'ALL /all/api': AllAPI
 }
